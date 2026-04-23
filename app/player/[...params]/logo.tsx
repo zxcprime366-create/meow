7d4a91ecbe652f5e17ea33fe1792b0d29af5f0d0
jerometecson-main <jerometecson.main@gmail.com>
2026-04-23 18:15:47 +0800

    meow

--- app/player/[...params]/logo.tsx
+++ app/player/[...params]/logo.tsx
@@ -10,9 +10,9 @@ export default function LoadingMetadata({ logo }: { logo: string }) {
       transition={{ duration: 0.3 }}
       className="fixed inset-0   overflow-hidden pointer-events-none"
     >
-      <div className="absolute left-[5%] -translate-x-[5%] sm:bottom-[10%] top-[50%] lg:top-[unset] sm:-translate-y-[10%] -translate-y-[50%] lg:max-w-md max-w-sm h-60 w-full   flex justify-center items-center ">
+      <div className="absolute left-[5%] -translate-x-[5%] sm:bottom-[10%] top-[50%] lg:top-[unset] sm:-translate-y-[10%] -translate-y-[50%]   spect-video   flex justify-center items-center ">
         <img
-          className="object-contain object-left h-full w-full drop-shadow-sm"
+          className="object-contain a object-left lg:max-w-md max-w-sm max-h-30 lg:max-h-50 drop-shadow-sm"
           src={`https://image.tmdb.org/t/p/w780/${logo}`}
           alt=""
         />
