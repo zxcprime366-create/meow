ad07b892dbcfd1f7a636aebe599f2f21c33abdf0
jerometecson-main <jerometecson.main@gmail.com>
2026-04-23 16:00:55 +0800

    xxxx

--- lib/server-list.ts
+++ lib/server-list.ts
@@ -3,26 +3,26 @@ import { ServerTypes } from "@/types/player-types";
 export const initialServers: ServerTypes[] = [
   {
     name: "Icarus I",
-    server: "icaruss",
+    server: "icarus",
     status: "queue",
     desc: "Vast Collection",
   },
 
   {
     name: "Atlas II",
-    server: "atlass",
+    server: "atlas",
     status: "queue",
-    desc: "4K Support & Multi Audios",
+    desc: "4K Support & Multi Audio",
   },
   {
     name: "Thanatos III",
-    server: "thanatoss",
+    server: "thanatos",
     status: "queue",
     desc: " Alternative",
   },
   {
     name: "Orion IV",
-    server: "orions",
+    server: "orion",
     status: "queue",
     desc: "Built-In Subtitle",
   },
